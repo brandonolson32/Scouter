df276ac187cefd541f20513f7f7eb7e9fb0a767a
brandonolson <brandonolson32@yahoo.com>
2019-05-16 15:24:29 -0400

    Model changes

--- app/src/main/java/com/example/scouter/model/Repository.java
+++ app/src/main/java/com/example/scouter/model/Repository.java
@@ -1,6 +1,6 @@
 package com.example.scouter.model;
 
-import com.example.scouter.entity.LifeForm;
+import com.example.scouter.entity.Character.LifeForm;
 import com.example.scouter.entity.User;
 
 import java.util.ArrayList;
@@ -31,12 +31,36 @@ public class Repository {
         lifeForms = new ArrayList<>();
     }
 
-    //User functions
     /**
      * get the user
      * @return the user of the app
      */
     public User getUser() {
         return user;
+    }
+
+    /**
+     * Gets the current User's id
+     * @return int the id of the user
+     */
+    public int getUserId() {
+        return user.getId(); }
+
+    public void addUser(User user) {
+        int id = Repository.getNextUniqueID();
+        user.setId(id);
+        lifeForms.add(user);
+    }
+
+    public void updateUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * Gets the User's name
+     * @return String the name of the User
+     */
+    public String getUserName() {
+        return user.getName();
     }
 }
